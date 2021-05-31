1b5a9c08fe3b3976baf80a51009f17913347db82
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-05-31 18:10:39 +0530

    Create worker-node-setup.sh

---
+++ worker-node-setup.sh
@@ -0,0 +1,40 @@
+yum install docker -y
+
+systemctl enable docker --now
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+ yum install -y  kubectl kubelet  kubeadm
+
+systemctl enable kubelet --now
+
+docker info | grep -i cgroup
+
+cat <<EOF | sudo tee  /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+
+systemctl restart docker
+
+docker info | grep -i cgroup
+
+yum install iproute-tc -
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
+
