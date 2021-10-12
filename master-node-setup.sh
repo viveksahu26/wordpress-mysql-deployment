a505bca897de3363fcfcbb8b07373b26f872631e
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-10-12 13:20:58 +0530

    configured yum and docker repo

--- master-node-setup.sh
+++ master-node-setup.sh
@@ -1,6 +1,38 @@
-yum install docker -y
+echo "Creating folder dvd and mounting to dev-cdrom"
+mkdir  /dvd
+mount  /dev/cdrom  /dvd/
 
+cat <<EOF | sudo tee -a /etc/rc.d/rc.local
+mount  /dev/cdrom  /dvd/
+EOF
+
+chmod +x  /etc/rc.d/rc.local
+
+echo "configuring repo for yum"
+cat <<EOF | sudo tee /etc/yum.repos.d/rhel.repo
+[AppStream]
+baseurl=file:///dvd/AppStream
+gpgcheck=0
+
+[BaseOS]
+baseurl=file:///dvd/BaseOS
+gpgcheck=0
+EOF
+
+yum repolist
+
+echo "configuring repo for docker"
+cat <<EOF | sudo tee /etc/yum.repos.d/docker.repo
+[docker]
+https://download.docker.com/linux/centos/7/x86_64/stable
+gpgcheck=0
+EOF
+
+echo "Installing docker now."
+yum install  docker-ce --nobest -y
 systemctl enable docker --now
+
+echo "configuring repo for kubernetes.."
 
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -12,6 +44,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+echo "Installing kubelet kubeadm kubectl"
 yum install -y kubelet kubeadm kubectl
 
 systemctl enable --now kubelet
