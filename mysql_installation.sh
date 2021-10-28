2a4bc72cebc737a9549681db603d4a83c4bb92dc
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-10-28 08:10:23 +0530

    Added mysql installation

---
+++ mysql_installation.sh
@@ -0,0 +1,14 @@
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+yum install wget
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+
+md5sum mysql57-community-release-el7-9.noarch.rpm
+rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+yum install mysql-server
+systemctl start mysqld
+systemctl status mysqld
+grep 'temporary password' /var/log/mysql/mysqld.log
+
+# Needs user input
+mysql_secure_installation
+mysqladmin -u root -p version
